39577f0ff167c057717d62d54db62fd8e0b23aaa
Trungthat2007 <ledoantrungthat0509@gmail.com>
2025-11-07 11:24:11 +0700

    Test commit

---
+++ src/ex51/TestLine.java
@@ -0,0 +1,14 @@
+package ex51;
+
+public class TestLine {
+    public static void main(String[] args) {
+        Line l1 = new Line(0, 0, 3, 4);
+        System.out.println(l1);
+
+        Point p1 = new Point(6,8);
+        Point p2 = new Point(9,10);
+        Line l2 = new Line(p1, p2);
+        System.out.println(l2);
+        System.out.println();
+    }
+}
